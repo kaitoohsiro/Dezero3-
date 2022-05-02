ccdbff478513ae18146a31465a116c8c43044634
kaitoohsiro <kaitooshiro7@gmail.com>
2022-05-02 13:22:09 +0900

    step2~24

---
+++ step/step02.py
@@ -0,0 +1,29 @@
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+
+import numpy as np
+# data = np.array(1.0)
+# x = Variable(data)
+# print(x.data)
+x = Variable(np.array(2.0))
+f = Square()
+y = f(x)
+
+print(type(y))
+print(y.data)
---
+++ step/step14study.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+
+            self.data = data
+            self.grad = None
+            self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def cleargrad(self):
+        self.grad = None
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            gys = (output.grad for output in f.outputs)
+            gxs = f.backward(*gys)
